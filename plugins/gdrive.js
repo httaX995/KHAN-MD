0c3375fd760e04d4c94f506c58addcfc5c72d8d3
httaX995 <httx995@gmail.com>
2025-02-20 13:02:27 +0530

    Update gdrive.js

--- plugins/gdrive.js
+++ plugins/gdrive.js
@@ -87,14 +87,14 @@ try{
   var [jid,link,name] = q.split(",");
  let res = await fg.GDriveDl(link)
   var name = name ? `${name.replace(/enter/g,'\n').replace(/oname/g,res.fileName)}` : res.fileName
-reply(`\n⬇️  *TC TEAM GDRIVE DOWNLOADER*  ⬇️
+reply(`\n⬇️  *CK CineMAX MVDL*  ⬇️
 
-*📃 File name:*  ${"🎬 DARKALPHAXTEAM 🎬\n"+name}
+*📃 File name:*  ${"🎬CK CineMAX🎬\n"+name}
 *💈 File Size:* ${res.fileSize}
 *🕹️ File type:* ${res.mimetype}
 
-*ᴛᴄ ᴛᴇᴀᴍ ᴍᴏᴠɪᴇᴅʟ*\n*ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴛᴇᴄʜɴɪᴄᴀʟ ᴄʏʙᴇʀꜱ*`)		
-conn.sendMessage(jid, { document: { url: res.downloadUrl }, fileName: "🎬 ᴛᴄ ᴍᴏᴠɪᴇ ᴅʟ 🎬\n"+name, mimetype: res.mimetype , caption : "\n"+name+"\n\n> *𝗧𝗘𝗖𝗛𝗡𝗜𝗖𝗔𝗟 𝗖𝗬𝗕𝗘𝗥𝗦*"})
+> 👨🏻‍💻 ᴍᴀᴅᴇ ʙʏ *ᴄʜᴇᴛʜᴍɪɴᴀ ᴋᴀᴠɪꜱʜᴀɴ*`)		
+conn.sendMessage(jid, { document: { url: res.downloadUrl }, fileName: "🎬CK CineMAX🎬\n"+name, mimetype: res.mimetype , caption : "*🍀ᴛɪᴛʟᴇ* ➠ "+name+"\n*🌟Qᴜᴀʟɪᴛʏ* ➠ 720p\n\n👨🏻‍💻 *ᴄʜᴇᴛʜᴍɪɴᴀ ᴋᴀᴠɪꜱʜᴀɴ*"})
 } catch (e) {
 reply('*Error..! Your Url is Private. Please Public It*')
 l(e)
